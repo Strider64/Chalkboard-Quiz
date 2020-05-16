325bd8a093c8d5e77c3ed5a0a97f475dd0f6f49f
John Pepp <jrpepp@pepster.com>
2020-05-16 07:05:08 -0400

    Javascript/Fetch of Username Duplicate Check
    05-16-2020

---
+++ checkName.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'assets/config/config.php';
+require_once "vendor/autoload.php";
+require_once 'loginFunctions.php';
+
+use Library\Users;
+use Library\Database as DB;
+
+$db = DB::getInstance();
+$pdo = $db->getConnection();
+
+/*
+ * The below must be used in order for the json to be decoded properly.
+ */
+$data = json_decode(file_get_contents('php://input'), true);
+
+
+function duplicateUsername($username, $pdo) {
+
+    try {
+        $query = "SELECT 1 FROM members WHERE username = :username";
+        $stmt = $pdo->prepare($query);
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row) {
+            return true; // userName is in database table
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+
+/*
+ * After converting data array to JSON send back to javascript using
+ * these functions.
+ */
+
+function errorOutput($output, $code = 500) {
+    http_response_code($code);
+    echo json_encode($output);
+}
+
+
+function output($output) {
+    http_response_code(200);
+    echo json_encode($output);
+}
